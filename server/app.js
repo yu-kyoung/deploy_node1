c3bf8deb3879691d946ff39911208ef0cab4bc46
user <px@px.com>
2025-08-21 10:28:15 +0900

    project init

---
+++ server/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config({ path: "./database/dbConfig.env" });
+const express = require("express");
+const app = express();
+const port = 3000;
+
+console.log(process.env.DB_NAME);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
+
+app.get("/hello", (req, res) => {
+  console.log(req.url);
+  res.send("Hello World!");
+});
+
+app.get("/api/board", (req, res) => {
+  res.send({ title: "노드 api 서버 update!!!!" });
+});
+
+// vue.js build 이후
+
+const path = require("path");
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+//새로고침 - 에러처리 - 가장밑에 위치
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public", "index.html"));
+});
